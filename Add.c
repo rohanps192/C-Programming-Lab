e9eff2b9f83ee38fee7b00deb3b737efa9674b52
rohanps192 <rohanps192@gmail.com>
2026-07-20 23:08:31 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	int b;
+	printf("Enter the 1st number:");
+	scanf("%d",&a);
+	printf("Enter the 2nd number:";
+	scanf("%d",&b);
+	int sum,sub,mul,div;
+	sum=a+b;
+	printf("sum is : %d\n",sum);
+	sub=a-b;
+	printf("Differenc is : %d\n",sub);
+	mul=a*b;
+	printf("Product is : %d\n",mul);
+	div=a/b;
+	printf("Division is : %d\n",div);
+}
+
